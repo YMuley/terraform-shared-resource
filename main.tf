24d7c2490818ca3cad7bee0f01e92442bc6f6b99
YMuley <91797130+YMuley@users.noreply.github.com>
2023-10-11 13:48:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
   source  = "app.terraform.io/Motifworks/resource_Group/azurerm"
   version = "1.0.2"
-  resource_group_list = "testing-terraform"
+  resource_group_list = ["testing-terraform"]
 }
