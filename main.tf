5f02eae36ba19f20413fee2aa07630fb92d1c075
YMuley <91797130+YMuley@users.noreply.github.com>
2023-10-12 17:46:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,43 @@ module "resource_group" {
   source  = "app.terraform.io/Motifworks/resource_Group/azurerm"
   version = "1.0.5"
 
-  resource_group_list = "rg-name-1"
+  resource_group_list = [
+
+        {
+
+            name        = "rg-ddi-poc" 
+
+            location    = "eastus"
+
+            tags        = {
+
+                location      = "eastus"
+
+                subscription  = "iac-dev"
+
+                environment   = "poc"
+
+            }
+
+        },
+
+        {
+
+            name        = "rg-ddi-dev" 
+
+            location    = "westus"
+
+            tags        = {
+
+                location      = "westus"
+
+                subscription  = "iac-dev"
+
+                environment   = "dev"
+
+            }
+
+        }
+
+    ]
 }
