82b6cf8b2db6238951d65f19f279fb7bc820cdf6
vijayispiro <124652645+vijayispiro@users.noreply.github.com>
2023-10-12 16:41:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "resource_group" {
   source  = "app.terraform.io/Motifworks/resource_Group/azurerm"
-  version = "1.0.2"
-  resource_group_list = ["testing-terraform"]
+  version = "1.0.3"
+
+  resource_groups = {     "rg1" = "East US"     "rg2" = "West US"     "rg3" = "North Europe"   }
 }
