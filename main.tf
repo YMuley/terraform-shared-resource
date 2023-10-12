bb9816210855e773ff78529867ba528d14c0f6ab
YMuley <91797130+YMuley@users.noreply.github.com>
2023-10-12 17:39:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
 module "resource_group" {
   source  = "app.terraform.io/Motifworks/resource_Group/azurerm"
-  version = "1.0.4"
+  version = "1.0.5"
 
-  resource_groups = {     
-"rg1" = "East US" 
-"rg2" = "West US"  
-}
+  resource_group_list = "rg-name-1"
 }
